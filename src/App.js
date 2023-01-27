bcfab61f99df5a0087b428a9d9d0165d26280530
Lix <zinebbendjafer9@gmail.com>
2023-01-27 17:35:20 +0100

    checkbox done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
+import {
+  animated,
+  useSpring,
+  config,
+  useSpringRef,
+  useChain
+} from "react-spring";
+
 
 function App() {
 
@@ -7,6 +15,30 @@ function App() {
   const [tasks, setTasks] = useState([{task:"eat", select:"All"}])
   const [displayTasks, setDisplayTasks] = useState(false)
   const [checked, setChecked] = useState(false)
+
+  const checkboxAnimationRef = useSpringRef();
+  const checkboxAnimationStyle = useSpring({
+    backgroundColor: checked ? "#808" : "#fff",
+    borderColor: checked ? "#808" : "#ddd",
+    config: config.gentle,
+    ref: checkboxAnimationRef
+  });
+
+  const [checkmarkLength, setCheckmarkLength] = useState(null);
+
+  const checkmarkAnimationRef = useSpringRef();
+  const checkmarkAnimationStyle = useSpring({
+    x: checked ? 0 : checkmarkLength,
+    config: config.gentle,
+    ref: checkmarkAnimationRef
+  });
+
+  useChain(
+    checked
+      ? [checkboxAnimationRef, checkmarkAnimationRef]
+      : [checkmarkAnimationRef, checkboxAnimationRef],
+    [0, 0.1]
+  );
 
   
   function addTasks(){
@@ -93,9 +125,32 @@ return (
         <div className="all-tasks">
           {tasks.map(task=> (
             <div className="task" key={nanoid()}>
-              <button onClick={checkBtn} >Check</button>
+              <label>
+                <input type="checkbox" onChange={checkBtn} />
+                <animated.svg
+                style={checkboxAnimationStyle}
+                className={`checkbox ${checked ? "checkbox--active" : ""}`}
+                aria-hidden="true"
+                viewBox="0 0 15 11"
+                fill="none"
+                >
+                 <animated.path
+                    d="M1 4.5L5 9L14 1"
+                    strokeWidth="2"
+                    stroke="#fff"
+                    ref={(ref) => {
+                      if (ref) {
+                      setCheckmarkLength(ref.getTotalLength());
+                      }
+                        }}
+                       strokeDasharray={checkmarkLength}
+                       strokeDashoffset={checkmarkAnimationStyle.x}
+                />
+              </animated.svg>
+              </label>
               <p style={underline} >{task.task}</p>
-              <p>{task.select}</p>
+              <button>delete</button>
+              <button>edit</button>
             </div>
           ))}
         </div>
