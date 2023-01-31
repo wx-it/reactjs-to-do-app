bf36abcd8c9863b715e40483bc40d0e8cdfd05bb
Lix <zinebbendjafer9@gmail.com>
2023-01-31 17:37:30 +0100

    dropdown styling done

--- src/components/createTask/CreateTask.jsx
+++ src/components/createTask/CreateTask.jsx
@@ -12,7 +12,7 @@ const CreateTask = ({
     <div className="creating-tasks">
       <button onClick={() => (modalOpen ? close() : open())}>Add Task</button>
 
-      <div>
+      {/* <div>
         <button
           type="button"
           onClick={() => {
@@ -27,6 +27,29 @@ const CreateTask = ({
         <button type="button" onClick={() => filterStatus("complete")}>
           Complete
         </button>
+      </div> */}
+
+      <div className="sec-center">
+        <input
+          className="dropdown"
+          type="checkbox"
+          id="dropdown"
+          name="dropdown"
+        />
+        <label className="for-dropdown" htmlFor="dropdown">
+          Select category
+        </label>
+        <div className="section-dropdown">
+          <button
+            onClick={() => {
+              setTasks(data);
+            }}
+          >
+            All
+          </button>
+          <button onClick={() => filterStatus("incomplete")}>Incomplete</button>
+          <button onClick={() => filterStatus("complete")}>Complete</button>
+        </div>
       </div>
     </div>
   );
