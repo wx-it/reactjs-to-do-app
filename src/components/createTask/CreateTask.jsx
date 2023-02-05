ae0cae242172ef73f53db3a706b80f996c82caab
Lix <zinebbendjafer9@gmail.com>
2023-02-05 20:23:05 +0100

    filter select styling done

--- src/components/createTask/CreateTask.jsx
+++ src/components/createTask/CreateTask.jsx
@@ -66,6 +66,59 @@ const CreateTask = ({ modalOpen, open, close }) => {
         />
       </div>
 
+      <div className="sort">
+        <div className="sort-title">
+          <BsFilter />
+          <p>Filter</p>
+        </div>
+
+        <Select
+          options={sortOptions}
+          defaultValue={sortOptions[0]}
+          isSearchable={false}
+          styles={{
+            control: () => ({
+              width: "9.37rem",
+              fontSize: "12px",
+              padding: "0",
+              backgroundColor: "transparent",
+              border: "none",
+              color: "white",
+              outline: "none",
+            }),
+            dropdownIndicator: () => ({
+              display: "none",
+            }),
+            indicatorSeparator: () => ({
+              display: "none",
+            }),
+            menuList: () => ({
+              fontSize: "12px",
+              color: "white",
+              backgroundColor: "rgba(57, 60, 68, 1)",
+            }),
+            option: (baseStyles) => ({
+              ...baseStyles,
+              margin: "none",
+              height: "100%",
+            }),
+            singleValue: (baseStyles) => ({
+              ...baseStyles,
+              color: "white",
+            }),
+          }}
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: 0,
+            colors: {
+              ...theme.colors,
+              primary25: "rgba(33, 123, 207, 1)",
+              primary: "black",
+            },
+          })}
+        />
+      </div>
+
       <button onClick={() => (modalOpen ? close() : open())}>Add Task</button>
     </div>
   );
