de9e838b052b20c0bcfd44c0933c992c87615cd1
Lix <zinebbendjafer9@gmail.com>
2023-02-07 16:36:37 +0100

    cleanup

---
+++ src/components/tasks/Checkbox.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  animated,
+  useSpring,
+  config,
+  useSpringRef,
+  useChain,
+} from "react-spring";
+import { useState } from "react";
+
+const Checkbox = ({ task, darkMode, checkBtn }) => {
+  const checkboxAnimationRef = useSpringRef();
+  const checkboxAnimationStyle = useSpring({
+    backgroundColor: task.check
+      ? darkMode
+        ? "#181F28"
+        : "rgba(33, 123, 207, 1)"
+      : darkMode
+      ? "#181F28"
+      : "#fffff",
+    borderColor: task.check
+      ? darkMode
+        ? "#181F28"
+        : "rgba(33, 123, 207, 1)"
+      : "#525252",
+    config: config.gentle,
+    ref: checkboxAnimationRef,
+  });
+
+  const [checkmarkLength, setCheckmarkLength] = useState(null);
+
+  const checkmarkAnimationRef = useSpringRef();
+  const checkmarkAnimationStyle = useSpring({
+    x: task.check ? 0 : checkmarkLength,
+    config: config.gentle,
+    ref: checkmarkAnimationRef,
+  });
+
+  useChain(
+    task.check
+      ? [checkboxAnimationRef, checkmarkAnimationRef]
+      : [checkmarkAnimationRef, checkboxAnimationRef],
+    [0, 0.1]
+  );
+
+  return (
+    <>
+      <label>
+        <input type="checkbox" onChange={() => checkBtn(task.id)} />
+        <animated.svg
+          style={checkboxAnimationStyle}
+          className={darkMode ? "dark-check check" : "light-check check"}
+          aria-hidden="true"
+          viewBox="0 0 15 11"
+          fill="none"
+        >
+          <animated.path
+            d="M1 4.5L5 9L14 1"
+            strokeWidth="2"
+            stroke="#fff"
+            ref={(ref) => {
+              if (ref) {
+                setCheckmarkLength(ref.getTotalLength());
+              }
+            }}
+            strokeDasharray={checkmarkLength}
+            strokeDashoffset={checkmarkAnimationStyle.x}
+          />
+        </animated.svg>
+      </label>
+    </>
+  );
+};
+
+export default Checkbox;
--- src/components/tasks/Task.jsx
+++ src/components/tasks/Task.jsx
@@ -1,55 +1,15 @@
 import React from "react";
-import {
-  animated,
-  useSpring,
-  config,
-  useSpringRef,
-  useChain,
-} from "react-spring";
+
 import { MdDelete, MdCreate } from "react-icons/md";
-import { useState } from "react";
 import { motion } from "framer-motion";
 
 import "./tasks.css";
+import Checkbox from "./Checkbox";
 
 const Task = ({ checkBtn, removeTask, task, editTask, darkMode }) => {
   const underline = {
     textDecoration: task.check ? "line-through" : "none",
   };
-
-  const checkboxAnimationRef = useSpringRef();
-  const checkboxAnimationStyle = useSpring({
-    backgroundColor: task.check
-      ? darkMode
-        ? "#181F28"
-        : "rgba(33, 123, 207, 1)"
-      : darkMode
-      ? "#181F28"
-      : "#fffff",
-    borderColor: task.check
-      ? darkMode
-        ? "#181F28"
-        : "rgba(33, 123, 207, 1)"
-      : "#525252",
-    config: config.gentle,
-    ref: checkboxAnimationRef,
-  });
-
-  const [checkmarkLength, setCheckmarkLength] = useState(null);
-
-  const checkmarkAnimationRef = useSpringRef();
-  const checkmarkAnimationStyle = useSpring({
-    x: task.check ? 0 : checkmarkLength,
-    config: config.gentle,
-    ref: checkmarkAnimationRef,
-  });
-
-  useChain(
-    task.check
-      ? [checkboxAnimationRef, checkmarkAnimationRef]
-      : [checkmarkAnimationRef, checkboxAnimationRef],
-    [0, 0.1]
-  );
 
   return (
     <motion.div
@@ -68,29 +28,7 @@ const Task = ({ checkBtn, removeTask, task, editTask, darkMode }) => {
       exit={{ y: -30, opacity: 0 }}
     >
       <div>
-        <label>
-          <input type="checkbox" onChange={() => checkBtn(task.id)} />
-          <animated.svg
-            style={checkboxAnimationStyle}
-            className={darkMode ? "dark-check check" : "light-check check"}
-            aria-hidden="true"
-            viewBox="0 0 15 11"
-            fill="none"
-          >
-            <animated.path
-              d="M1 4.5L5 9L14 1"
-              strokeWidth="2"
-              stroke="#fff"
-              ref={(ref) => {
-                if (ref) {
-                  setCheckmarkLength(ref.getTotalLength());
-                }
-              }}
-              strokeDasharray={checkmarkLength}
-              strokeDashoffset={checkmarkAnimationStyle.x}
-            />
-          </animated.svg>
-        </label>
+        <Checkbox task={task} darkMode={darkMode} checkBtn={checkBtn} />
         <p style={underline}>{task.text}</p>
       </div>
       <div>
